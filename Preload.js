2b886de2e40e43d1d351d46a6d85e93879a97099
B. Sravan Kumar <76738791+sravankumar025@users.noreply.github.com>
2026-08-05 23:05:30 +0530

    Update Preload.js

--- Preload.js
+++ Preload.js
@@ -1,8 +1,13 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-    savePdf: () => ipcRenderer.invoke("generate-pdf"),
-    saveAdvice: (formData) => ipcRenderer.invoke("save-advice", formData),
-    getAdvice: (id) => ipcRenderer.invoke("get-advice", id),
-    deleteAdvices: () => ipcRenderer.invoke("delete-advices"),
+  savePdf: () => ipcRenderer.invoke("generate-pdf"),
+  saveAdvice: (formData) => ipcRenderer.invoke("save-advice", formData),
+  getAdvice: (id) => ipcRenderer.invoke("get-advice", id),
+  deleteAdvices: () => ipcRenderer.invoke("delete-advices"),
+  onDownloadStatus: (callback) => {
+    const subscription = (event, value) => callback(value);
+    ipcRenderer.on("download-status", subscription);
+    return () => ipcRenderer.removeListener("download-status", subscription);
+  },
 });
