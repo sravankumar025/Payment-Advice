6d4cecf06f93d443b6ff9a823c77ebbb432f1f6b
B. Sravan Kumar <76738791+sravankumar025@users.noreply.github.com>
2026-08-07 17:46:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import PaymentAdviceForm from './Pages/PaymentAdviceForm'
-
+import PrintableInvoice from './Pages/PrintableInvoice'
+import Dashboard from './Pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 function App() {
 
   return (
     <>
-      <PaymentAdviceForm />
+      <Router>
+        <Routes>
+          <Route path="/" element={<PaymentAdviceForm/>}/>
+          <Route path="/reports" element={<Dashboard/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
